a9622eb0967d576e1da5dffd17079d156b6b622b
zero89854-hue <zero89854@gmail.com>
2026-06-07 23:33:17 +0800

    使用 Colab 建立

---
+++ 20260607_py.py
@@ -0,0 +1,47 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOvWPiv8jl+2T53v743/VIv",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/zero89854-hue/20260602/blob/main/20260607_py.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "8tKd8g8sq_qf"
+      },
+      "outputs": [],
+      "source": [
+        "date=int(input(\"輸入今天日期\"))\n",
+        "answer=20260607\n",
+        "if date==answer:\n",
+        "  print(\"正確\")\n",
+        "else:\n",
+        "  print(\"錯誤\")\n",
+        ""
+      ]
+    }
+  ]
+}
